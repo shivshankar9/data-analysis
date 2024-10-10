c3c1f67a1eeb46483c661764ede2983d1b6c8f2a
shivshankar9 <shivshankarkumar281@gmail.com>
2024-10-11 00:02:48 +0530

    a

---
+++ server/src/app.ts
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import passport from "passport";
+import session from "express-session";
+import MongoStore from "connect-mongo";
